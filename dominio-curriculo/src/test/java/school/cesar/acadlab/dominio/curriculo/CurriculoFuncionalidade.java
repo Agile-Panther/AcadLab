d801cad3ceeb8ea31872e2c4af9cd2e6ae336d48
JuliaTBarros <juliatbarros05@gmail.com>
2026-06-14 16:09:00 -0300

    feat: adiciona infraestrutura de testes e repositório em memória

---
+++ dominio-curriculo/src/test/java/school/cesar/acadlab/dominio/curriculo/CurriculoFuncionalidade.java
@@ -0,0 +1,9 @@
+package school.cesar.acadlab.dominio.curriculo;
+
+public class CurriculoFuncionalidade {
+    protected CurriculoRepositorioTest repositorio;
+
+    public CurriculoFuncionalidade() {
+        repositorio = new CurriculoRepositorioTest();
+    }
+}
