d06f48b3a532157ce85bc5fab4d5038fc00ca69e
vinimarques7 <vini.marques_07@outlook.com>
2026-06-13 21:07:00 -0300

    feat: adiciona agregado e value objects de mobilidade acadêmica

---
+++ dominio-mobilidade-academica/src/main/java/school/cesar/acadlab/dominio/mobilidadeacademica/mobilidade/EstudanteId.java
@@ -0,0 +1,29 @@
+package school.cesar.acadlab.dominio.mobilidadeacademica.mobilidade;
+
+import static org.apache.commons.lang3.Validate.isTrue;
+import java.util.Objects;
+
+public class EstudanteId {
+    private final int id;
+
+    public EstudanteId(int id) {
+        isTrue(id > 0, "O id do estudante deve ser positivo");
+        this.id = id;
+    }
+
+    public int getId() { return id; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof EstudanteId other) {
+            return id == other.id;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(id); }
+
+    @Override
+    public String toString() { return Integer.toString(id); }
+}
